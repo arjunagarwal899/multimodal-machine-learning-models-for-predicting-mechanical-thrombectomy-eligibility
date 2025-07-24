67b013391a39b65ab6699d0e23893e02a11458ea
Arjun Agarwal <arjun.agarwal@qure.ai>
2025-07-24 04:51:15 +0000

    split master into master and qer

--- mt_eligibility/load.py
+++ mt_eligibility/load.py
@@ -3,11 +3,13 @@ import os
 import pandas as pd
 
 
-def load_and_combine_csvs(csv_names: set = {"master", "annotations", "data_split"}) -> pd.DataFrame:
+def load_and_combine_csvs(csv_names: set = {"master", "qer", "annotations", "data_split"}) -> pd.DataFrame:
     # Load necessary CSV files
     csvs = []
     if "master" in csv_names:
         csvs.append(pd.read_csv(os.path.join(os.path.dirname(__file__), r"data/master.csv")))
+    if "qer" in csv_names:
+        csvs.append(pd.read_csv(os.path.join(os.path.dirname(__file__), r"data/qer.csv")))
     if "annotations" in csv_names:
         csvs.append(pd.read_csv(os.path.join(os.path.dirname(__file__), r"data/annotations.csv")))
     if "data_split" in csv_names:
